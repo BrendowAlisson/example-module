9624186aabb1d833368bd17a3f8ea12c75ac014d
BrendowAlisson <brendowalves@hotmail.com>
2024-08-18 08:32:57 -0300

    first commit

---
+++ include/multiply.h
@@ -0,0 +1,6 @@
+#ifndef MULTIPLY_H__
+#define MULTIPLY_H__
+
+int multiply_by_2(int value);
+
+#endif
---
+++ src/multiply.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "multiply.h"
+
+int multiply_by_2(int value) 
+{
+    return value*2;
+}
